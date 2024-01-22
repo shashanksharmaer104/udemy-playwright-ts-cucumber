e603be53d57cf656895f1a37c2c4299c3dd8a5d6
Shashank Sharma <shashanksharma.er104@gmail.com>
2024-01-22 16:09:59 +0530

    Added new test case for click scenario

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,3 +6,11 @@ test('Simple sample script',async ( {page} ) => {
     const locator = await page.locator('h1')
     await expect(locator).toHaveText('Example Domain')
 })
+
+test('Click on element',async ({ page }) => {
+    await page.goto("http://zero.webappsecurity.com/index.html")
+    await page.click("#signin_button")
+    await page.click('text=Sign in')
+    const errorMessage = await page.locator('.alert-error');
+    await expect(errorMessage).toContainText('Login and/or password are wrong.')
+})
